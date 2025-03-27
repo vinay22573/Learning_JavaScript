743836363947d3bc7d190b3e651a8aaad7720f4d
Vinay Kumar Dubey <119327266+vinay22573@users.noreply.github.com>
2025-03-27 14:35:39 +0000

    chore: dataTypeSummary

--- 01_basics/04_dataTypesSummary.js
+++ 01_basics/04_dataTypesSummary.js
@@ -15,13 +15,13 @@
 // let accountBalance = 10000.00;
 // let name = "Vinay";
 // let accountNumber = "Vinay22573";
-// bcause u can see here just using let we are defining all the variables and assigning them values without even caring about what type of data it is.
+// // bcause u can see here just using let we are defining all the variables and assigning them values without even caring about what type of data it is.
 
-const outsideTemp = null;
+// const outsideTemp = null;
 
-let userEmail;
-const id = Symbol("123");
-const anotherId = Symbol("123");
+// let userEmail;
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
 // console.log(id == anotherId);
 // console.log(id);
 // console.log(anotherId);
@@ -29,35 +29,36 @@ const anotherId = Symbol("123");
 
 // const bigNumber = 8901234567890n;
 // just use n after a number to make it bigint
-// console.log(typeof bigNumber);
+// console.log(typeof bigNumber);//its undefined
 
 
 
-// Arrays
-const heroes = ['Shaktimaan','Atal Bihari Vajpayee', 'Manoj Pandey', 'Bhagat Singh', 'Vikram Batra' ];
-console.log(heroes);
+// // Arrays
+// const heroes = ['Shaktimaan','Atal Bihari Vajpayee', 'Manoj Pandey', 'Bhagat Singh', 'Vikram Batra' ];
+// console.log(heroes);
 
 
 
-// Objects
-let myObj = {
-    name: "Vinay",
-    age: 21,
-    isAdult: true,
-    isLoggedIn: false,
-    accountBalance: 10000.00,
-    accountNumber: "Vinay22573"
-}
-// anything which is under these curly braces is an object
-console.log(myObj);
-console.log(typeof myObj);//object
-console.log(myObj.name);//Vinay
-console.log(myObj.age);//21
-console.log(typeof myObj.accountBalance);//number
+// // Objects
+// let myObj = {
+//     name: "Vinay",
+//     age: 21,
+//     isAdult: true,
+//     isLoggedIn: false,
+//     accountBalance: 10000.00,
+//     accountNumber: "Vinay22573"
+// }
+// // anything which is under these curly braces is an object
+// console.log(myObj);
+// console.log(typeof myObj);//object
+// console.log(myObj.name);//Vinay
+// console.log(myObj.age);//21
+// console.log(typeof myObj.accountBalance);//number
 
 
-// Functions
-// so we can see here that function too can be 
-const myFunction =  myFunc(){
-    console.log("Jai Hind");
-}
+// // Functions
+// // so we can see here that function too can be 
+// const myFunction =  function(){
+//     console.log("Jai Hind");
+// }
+// console.log(typeof myFunction);// though it will show function but its object I mean the function itself is object
